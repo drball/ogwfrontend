eeb2b5dc00281aecc1beab86bbbec46641141cc5
david <david@ongoingworlds.com>
2014-05-24 17:16:51 +0100

    Starting on game homepage

--- game-home.php
+++ game-home.php
@@ -15,9 +15,22 @@
                 </div>
 
                 <div class="col-xs-12 col-sm-9 col-md-7 main game-post infinite-height">
-                    <div class="game-bg-image" style="background:url(images/heavyrain-pbem.jpg)"></div>
+                    <div class="game-bg-image game-bg-image--full" style="background:url(images/heavyrain-pbem.jpg)"></div>
 
                     <h1>JMC Blue Dwarf <span class="hidden">- a play-by-post roleplaying game</span></h1>
+
+                    <div class="row">
+                        <div class="col-md-9">
+                            <div class="game-summary" style="display: block;">
+                                <p>Hero High is a high school where young super humans are taught how to use their powers.  This establishment was created back in the fifties to help super humans learn to control their powers after one to many incidents.  Once a child is identified as a super human they are automatically enrolled by <span class="read-more" style="display: inline;">… <a href="#">Show More</a></span></p>
+                            </div>
+                        </div>
+
+                        <div class="col-md-3">
+
+                        </div>
+                    </div>
+
 
 
                 </div>
