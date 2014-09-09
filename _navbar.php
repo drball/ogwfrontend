e266d901237418f39219ebeb6f4c21250ef5e3f3
david <david@ongoingworlds.com>
2014-09-09 20:27:18 +0100

    changing things up a bit, and restarting (mostly) based on some sketches I did. Started on the widescreen "myprofile" template

--- _navbar.php
+++ _navbar.php
@@ -1,25 +1,30 @@
-<div class="navbar navbar-inverse" role="navigation">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">OngoingWorlds</a>
+<img src="/images/monster.png" width="311" height="278" alt="A monster holding the Earth in it's tentacles" class="logo-monster--medium">
+
+<div class="navbar navbar-inverse container-wrapper" role="navigation">
+
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">OngoingWorlds</a>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="active"><a href="#">Roleplaying games</a></li>
+                    <li><a href="#about">Create game</a></li>
+                    <li><a href="#contact">Login</a></li>
+                </ul>
+
+                <form class="navbar-form navbar-right">
+                    <input type="text" class="form-control" placeholder="Search games...">
+                </form>
+
+            </div><!--/.nav-collapse -->
         </div>
-        <div class="navbar-collapse collapse">
-            <ul class="nav navbar-nav navbar-right">
-                <li class="active"><a href="#">Roleplaying games</a></li>
-                <li><a href="#about">Create game</a></li>
-                <li><a href="#contact">Login</a></li>
-            </ul>
 
-            <form class="navbar-form navbar-right">
-                <input type="text" class="form-control" placeholder="Search games...">
-            </form>
-
-        </div><!--/.nav-collapse -->
-    </div>
 </div>
+
---
+++ myprofile.php
@@ -0,0 +1,48 @@
+<?php include("_top.php") ?>
+
+<?php include("_navbar.php") ?>
+
+
+    <div class="stripe">
+        <div class="container-wrapper">
+            <div class="container-fluid">
+                <h1 class="main-title--avoid-logo">Onion's public profile</h1>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="container-wrapper">
+        <div class="container-fluid">
+
+            <div class="row">
+
+                <div class="col-xs-12 main">
+
+                    <h1>JMC Blue Dwarf <span class="hidden">- a play-by-post roleplaying game</span></h1>
+
+                    <div class="row">
+                        <div class="col-md-9">
+                            <div class="game-summary" style="display: block;">
+                                <p>Hero High is a high school where young super humans are taught how to use their powers.  This establishment was created back in the fifties to help super humans learn to control their powers after one to many incidents.  Once a child is identified as a super human they are automatically enrolled by <span class="read-more" style="display: inline;">… <a href="#">Show More</a></span></p>
+                            </div>
+                        </div>
+
+                        <div class="col-md-3">
+
+                        </div>
+                    </div>
+
+
+
+                </div>
+
+
+            </div>
+
+        </div><!-- /.container -->
+    </div>
+
+
+
+<?php include("_bottom.php") ?>
